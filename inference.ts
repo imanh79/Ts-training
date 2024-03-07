b1976f91bcabc6722b55caee3904d1f221fd7664
Iman Haghi❄️ <93634742+imanh79@users.noreply.github.com>
2024-03-07 12:22:08 +0330

    Delete inference.ts

--- inference.ts
+++
@@ -1,5 +0,0 @@
-// Inference type
-// استنتاج تایپ در تایپ اسکریپت این امکان رو دارد تا انواع داده ای رو بدون مشخص کردن آن  تشخیص دهد
-let num = 1; //number
-let str = "iman"; //string
-let obj = {}; //object
