dd9c81089f037f19d2df44b11c9d7a8dfbaa53a5
Iman Haghi❄️ <93634742+imanh79@users.noreply.github.com>
2024-03-07 15:42:22 +0330

    Union.ts

--- union.ts
+++ union.ts
@@ -1,6 +1,7 @@
 // union type
 //توصیف کردن مقداری که میتواند چند تایپ داشته باشد
 //نماد = > (|)
+// معنی لغوی : انحاد
 function getuser(data: null | undefined): null | object[] {
     return null
 }
