4fbb1736ac6217a56c3c2245feb087722d6e39c5
Iman Haghi❄️ <93634742+imanh79@users.noreply.github.com>
2024-03-07 12:22:59 +0330

    Inference type

---
+++ inference.ts
@@ -0,0 +1,5 @@
+// Inference type
+// استنتاج تایپ در تایپ اسکریپت این امکان رو دارد تا انواع داده ای رو بدون مشخص کردن آن  تشخیص دهد
+let num = 1; //number
+let str = "iman"; //string
+let obj = {}; //object
